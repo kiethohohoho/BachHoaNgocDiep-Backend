a8eb468fe8de1b2f85702011184c1db9590ef180
Le Kiet <kiethohohoho@gmail.com>
2023-03-29 21:18:34 +0700

    Open docs api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authRoute = require('./auth.route');
 // const userRoute = require('./user.route');
 const docsRoute = require('./docs.route');
-const config = require('../../config/config');
+// const config = require('../../config/config');
 
 const router = express.Router();
 
@@ -30,10 +30,13 @@ defaultRoutes.forEach((route) => {
 });
 
 /* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
+// if (config.env === 'development') {
+//   devRoutes.forEach((route) => {
+//     router.use(route.path, route.route);
+//   });
+// }
+devRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
 
 module.exports = router;
