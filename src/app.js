af6135538b3f088b4793d9745ead32d98641560a
Le Kiet <kiethohohoho@gmail.com>
2023-05-24 16:29:35 +0700

    change 404 respone message

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.use('/v1', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+  next(new ApiError(httpStatus.NOT_FOUND, 'Đường dẫn không tồn tại!'));
 });
 
 // convert error to ApiError, if needed
