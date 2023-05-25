da67dfb1306f083ae186891c2ec7a67f6d78ff68
Le Kiet <kiethohohoho@gmail.com>
2023-05-25 10:52:41 +0700

    fix host mssql

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const config = require('./config');
 
 const sequelize = new Sequelize('BachHoaNgocDiep', 'sa', 'b4chH04Ng0cD13p', {
-  host: config.env !== 'production' ? 'bachhoangocdiep.com' : 'db',
+  host: config.env !== 'production' ? '116.118.51.72' : 'db',
   port: 1433,
   dialect: 'mssql',
   dialectOptions: {
