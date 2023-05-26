e36b30fa2e55699c6438d5767f08768a2b561e93
Le Kiet <kiethohohoho@gmail.com>
2023-05-26 17:04:15 +0700

    abc

--- src/routes/v1/analyst.route.js
+++ src/routes/v1/analyst.route.js
@@ -24,48 +24,13 @@ module.exports = router;
 
 /**
  * @swagger
- * /carts:
+ * /analyst/revenue:
  *   get:
- *     summary: Lấy danh sách sản phẩm trong giỏ hàng (search, sort, filter, pagination)
- *     description: Cho phép search, sort, multi filter, phân trang /carts?search=a&sort=Price,Name&order=asc,desc&filter[Price][gt]=1&filter[Name][eq]=abc&page=1&limit=20.
+ *     summary: Lấy thống kê tổng tiền các đơn hàng trong ngày, tuần, tháng hiện tại
+ *     description: Lấy thống kê tổng tiền các đơn hàng trong ngày, tuần, tháng hiện tại
  *     tags: [Analyst]
  *     security:
  *       - bearerAuth: []
- *     parameters:
- *       - name: search
- *         in: query
- *         schema:
- *           type: string
- *       - name: filter
- *         in: query
- *         schema:
- *           type: object
- *           properties:
- *             fieldName:
- *               type: object
- *               properties:
- *                 operator:
- *                   type: string
- *       - name: sort
- *         in: query
- *         schema:
- *           type: array
- *           items:
- *             type: string
- *       - name: order
- *         in: query
- *         schema:
- *           type: array
- *           items:
- *             type: string
- *       - name: page
- *         in: query
- *         schema:
- *           type: number
- *       - name: limit
- *         in: query
- *         schema:
- *           type: number
  *     responses:
  *       "200":
  *         description: OK
@@ -74,25 +39,6 @@ module.exports = router;
  *             schema:
  *               type: object
  *               properties:
- *                 Data:
- *                   type: array
- *                   items:
- *                     $ref: '#/components/schemas/Cart'
- *                 Pagination:
- *                   type: object
- *                   properties:
- *                     TotalCount:
- *                       type: integer
- *                       example: 1
- *                     TotalPages:
- *                       type: integer
- *                       example: 1
- *                     CurrentPage:
- *                       type: integer
- *                       example: 1
- *                     Limit:
- *                       type: integer
- *                       example: 10
  *       "401":
  *         $ref: '#/components/responses/Unauthorized'
  *       "403":
